b8f134a41f397e33f13a2a3927346d9814dd7b0e
TAFE\2101560216 <eternumhd111@gmail.com>
2018-06-07 09:51:04 +1000

    *Added database connect for clientDataLite_GUI*

--- EITS_Office_Systems/src/clientDataLite_GUI.java
+++ EITS_Office_Systems/src/clientDataLite_GUI.java
@@ -1,3 +1,9 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,12 +15,16 @@
  * @author 2104990817
  */
 public class clientDataLite_GUI extends javax.swing.JFrame {
-
+    Connection con = null; 
+    ResultSet rs = null; 
+    PreparedStatement ps = null; 
+    Statement st = null; 
     /**
      * Creates new form clientDataLite_GUI
      */
     public clientDataLite_GUI() {
         initComponents();
+        con = DatabaseConnection.getConnection();
     }
 
     /**
